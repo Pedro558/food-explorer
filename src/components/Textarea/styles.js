38ec7d0f415cb6ba1b56484a27dbeae90b2bd241
pedro558 <pedroafonso109@gmail.com>
2023-06-26 17:39:20 -0300

    :lipstick: style: change backgroundColor

--- src/components/Textarea/styles.js
+++ src/components/Textarea/styles.js
@@ -15,11 +15,11 @@ export const TextareaInput= styled.textarea`
   font-family: 'Roboto', sans-serif;
   font-weight: 400;
   font-size: 1.6rem;
-  color: ${({ theme }) => theme.COLORS.WHITE};
+  color: ${({ theme }) => theme.COLORS.GRAY_200};
   padding: 1.4rem;
-  border: 1px solid ${({ theme }) => theme.COLORS.WHITE};
+  border: none;
   border-radius: 0.5rem;
-  background: transparent;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_400};
   &::placeholder {
     color: ${({ theme }) => theme.COLORS.GRAY_500};
     font-size: 1.6rem;
